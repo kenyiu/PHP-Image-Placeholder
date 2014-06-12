9f3deadb7b366d4c0321774bb299291f61b3d2fd
Ken Yiu <pakhin.yiu@gmail.com>
2014-06-13 03:37:35 +0800

    Add plain text option as default

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 PHP-Image-Placeholder
 =====================
 
-Script creating a placeholder with pictures from Wikimedia Commons
+Script creating a placeholder:
+1. Pictures from Wikimedia Commons (Pretty, yet slower), or
+2. Plain text with plain background (Boring, yet faster)
 
 ##Usage
 ```
-<img src='placeholder.php' alt='testing'> <!--Default width and height : 200 x 200!-->
-<img src='placeholder.php?w=300&h=300' alt='testing'> <!--Customized width and height!-->
+<img src='palceholder.php' alt='testing'> <!-- Text, Default width and height : 200 x 200 !-->
+
+<img src='placeholder.php?w=300&h=300' alt='testing'><!--Text, Customized width and height!-->
+
+<img src='placeholder.php?t=image' alt='testing'> <!--Image, Default width and height : 200 x 200!-->
+
+<img src='placeholder.php?t=image&w=300&h=300' alt='testing'> <!--Image, Customized width and height!-->
 ```
 ##Technical Information
 PHP-Image-Placeholder is developed on:
--- placeholder.php
+++ placeholder.php
@@ -1,6 +1,12 @@
 <?php
-require_once 'goutte.phar';
+require 'goutte.phar';
 use Goutte\Client;
+
+if (isset($_GET['t']) && $_GET['t']=="image") {
+	$type = "image";
+} else {
+	$type = "text";
+}
 
 if (isset($_GET['w']) && is_numeric($_GET['w'])) {
     $width = $_GET['w'];
@@ -14,17 +20,46 @@ if (isset($_GET['h']) && is_numeric($_GET['h'])) {
     $height = 200;
 }
 
-$url = 'http://commons.wikimedia.org/wiki/Special:Random/File';
+if ($type == "image") {
+	$url = 'http://commons.wikimedia.org/wiki/Special:Random/File';
 
-$client = new Client();
-$crawler = $client->request('GET', $url);
+	$client = new Client();
+	$crawler = $client->request('GET', $url);
 
-$status_code = $client->getResponse()->getStatus();
-if($status_code==200){
-    //echo "<pre>";
-    $images = new Imagick($crawler->filter('div.fullMedia > a')->first()->link()->getUri());
-    $images->scaleImage($width, $height);
-    header("Content-Type: image/png");
-    echo $images;
+	$status_code = $client->getResponse()->getStatus();
+	if($status_code==200){
+	    //echo "<pre>";
+	    if (!extension_loaded('imagick')) {
+	    	echo 'ImageMagick not installed';
+	    	exit();
+	    }
+	    $image = new Imagick($crawler->filter('div.fullMedia > a')->first()->link()->getUri());
+	    $image->scaleImage($width, $height);
+	}
+
+} else {
+	$image = new Imagick();
+	$draw = new ImagickDraw();
+	$pixel = new ImagickPixel('black');
+
+	/* New image */
+	$image->newImage($width, $height, $pixel);
+
+	/* White text */
+	$draw->setFillColor('white');
+
+	/* Font properties */
+	$draw->setFontSize(50);
+
+	/* Create text */
+	$draw->setTextAlignment(2);
+	$draw->annotation($width/2, $height/2, $width."x".$height);
+	$image->drawImage($draw);
+
+	/* Give image a format */
+	$image->setImageFormat('png');
 }
+
+header("Content-Type: image/png");
+echo $image;
 ?>
